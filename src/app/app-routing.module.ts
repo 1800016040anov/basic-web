f5f2df385b2d62e7de3c50b463ade0b7afee0ff0
1800016040anov <a1800016040@webmail.uad.ac.id>
2020-04-01 15:28:15 +0700

    praktikum2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+const routes: Routes = [
+	{ 
+		path : 'home',
+		component: HomeComponent
+	},
+	{
+	path : 'Students',
+	component: StudentsComponent
+	},
+	{
+	path : 'teacher',
+	component : TeacherComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentsComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule,
